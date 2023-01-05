ac3965488bed26d2ee8584eadb5b665c623fe8a0
Marcos Sousa <96206825+marcossantossousa@users.noreply.github.com>
2023-01-04 21:06:26 -0300

    ....

--- src/components/Heroimg.tsx
+++ src/components/Heroimg.tsx
@@ -3,7 +3,7 @@ import "./Hero.module.css";
 export const Heroimg = () => {
   return (
     <div className="hero">
-      <div className="mask"></div>
+      <div className="mask">apaga aqui</div>
     </div>
   );
 };
